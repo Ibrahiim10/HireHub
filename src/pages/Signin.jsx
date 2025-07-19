cd3e8674ea62e132a6260813c1a0ffe9bec408cc
Ibrahiim10 <ustadibrahim00@gmail.com>
2025-07-19 21:12:36 +0300

    Sign In with Profile Creation

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { useAuth } from '../context/AuthContext';
+import { signInUser } from '../lib/auth';
 
 const Signin = () => {
-  // handle submit
-  //   const handleSubmit = async (event) => {
-  //     event.preventDefault();
-  //     setIsLoading(true);
-  //     setError(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  //     try {
-  //       await signInUser(email, password);
-  //       navigate('/');
-  //     } catch (error) {
-  //       setError(
-  //         error.message || 'Failed to sign in. Please check your credentials.'
-  //       );
-  //       console.log('error', error);
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   };
+  const authIfo = useAuth();
+  console.log({ authIfo });
+
+  const navigate = useNavigate();
+
+  // handle submit
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      await signInUser(email, password);
+      navigate('/');
+    } catch (error) {
+      setError(
+        error.message || 'Failed to sign in. Please check your credentials.'
+      );
+      console.log('error', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
@@ -32,12 +44,12 @@ const Signin = () => {
 
           {/* form info */}
           <div className="bg-white rounded-lg shadow-md p-8">
-            {/* {error && (
+            {error && (
               <div className="mb-4 p-3 bg-red-100 text-red-700 rounded-md text-sm">
                 {error}
               </div>
-            )} */}
-            <form>
+            )}
+            <form onSubmit={handleSubmit}>
               <div className="mb-6">
                 <label
                   className="block text-gray-700 text-sm font-semibold mb-2 "
@@ -50,10 +62,10 @@ const Signin = () => {
                   type="email"
                   id="email"
                   placeholder="your@email.com"
-                  //   value={email}
-                  //   onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
-                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
 
@@ -69,11 +81,11 @@ const Signin = () => {
                   type="password"
                   id="password"
                   placeholder="******"
-                  //   value={password}
-                  //   onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   minLength={6}
-                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                  className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
                 <p className="text-xs text-gray-500 mt-1">
                   Must be at least 6 characters
@@ -82,20 +94,19 @@ const Signin = () => {
 
               <button
                 type="submit"
-                className="w-full bg-orange-600 hover:bg-orange-700 text-white font-bold py-3 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-opacity-50 transition duration-200 disabled:cursor-not-allowed"
-                //   disabled={isLoading}
+                className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-200 disabled:cursor-not-allowed"
+                disabled={isLoading}
               >
-                {/* {isLoading ? 'Signing in...' : 'Sign In'} */}
-                Sign In
+                {isLoading ? 'Signing in...' : 'Sign In'}
               </button>
             </form>
 
             <div className="text-center mt-6 ">
               <p className="text-gray-600 text-sm">
-                {/* Don't have an account?{' '} */}
+                Don't have an account?{' '}
                 <Link
                   to="/signup"
-                  className="text-orange-600 hover:text-orange-800 font-semibold"
+                  className="text-blue-600 hover:text-blue-800 font-semibold"
                 >
                   Sign Up
                 </Link>
